55c090a5a99c83b92d862750a559a63ffab37c75
adefoluso <adedejifoluso@gmail.com>
2023-10-08 17:05:45 +0100

    FEAT:create event

--- src/Controllers/event.controller.ts
+++ src/Controllers/event.controller.ts
@@ -2,7 +2,42 @@ import { PrismaClient } from '@prisma/client'
 import { Request, Response } from 'express'
 const prisma = new PrismaClient()
 
-const createEvent = (req: Request, res: Response) => {}
+const createEvent = async(req: Request, res: Response) => {
+  const { originalname, mimetype, size, path } = req.file;
+ 
+  const imageUploadResponse = {
+    secure_url: req.file.path
+  };
+  try {
+    const {
+      created_by,
+      event_name,
+      event_description,
+      // image,
+      event_start,
+      event_end,
+      location,
+    } = req.body;
+
+    const newEvent = await prisma.event.create({
+      data: {
+        created_by,
+        event_name,
+        event_description,
+        image: imageUploadResponse.secure_url,
+        event_start,
+        event_end,
+        location,
+      },
+    });
+
+    res.status(201).json(newEvent);
+  } catch (error) {
+  
+    console.error("Error creating event:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
 
 const getAllEvents = (req: Request, res: Response) => {}
 
