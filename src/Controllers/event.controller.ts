3bb4b00f33ecff0decc7f21299f8f6e81d37803c
dyCodes <yusufdaudu51@gmail.com>
2023-10-08 14:25:17 +0100

    feat: Implement getAllEvents

--- src/Controllers/event.controller.ts
+++ src/Controllers/event.controller.ts
@@ -1,15 +1,23 @@
-import { PrismaClient } from '@prisma/client'
-import { Request, Response } from 'express'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+const prisma = new PrismaClient();
 
-const createEvent = (req: Request, res: Response) => {}
+const createEvent = (req: Request, res: Response) => {};
 
-const getAllEvents = (req: Request, res: Response) => {}
+const getAllEvents = async (req: Request, res: Response) => {
+	// Get all events
+	const events = await prisma.event.findMany();
+	if (events.length > 0) {
+		res.status(200).json(events);
+	} else {
+		res.status(404).json({ error: 'No events found' });
+	}
+};
 
-const getFriendEvent = (req: Request, res: Response) => {}
+const getFriendEvent = (req: Request, res: Response) => {};
 
-const eventSearch = (req: Request, res: Response) => {}
+const eventSearch = (req: Request, res: Response) => {};
 
-const getEventById = (req: Request, res: Response) => {}
+const getEventById = (req: Request, res: Response) => {};
 
-export { createEvent, getAllEvents, getFriendEvent, eventSearch, getEventById }
+export { createEvent, getAllEvents, getFriendEvent, eventSearch, getEventById };
