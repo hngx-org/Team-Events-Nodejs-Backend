0e12147fbfa552ed7853b7062bf8d5ba1fc28a69
dyCodes <yusufdaudu51@gmail.com>
2023-10-10 16:52:14 +0100

    chore: build application

---
+++ dist/Controllers/comment.controller.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getComments = exports.createComment = void 0;
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const createComment = (req, res) => { };
+exports.createComment = createComment;
+const getComments = (req, res) => { };
+exports.getComments = getComments;
+//# sourceMappingURL=comment.controller.js.map
---
+++ dist/Controllers/group.controller.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getGroupEvent = exports.getGroupById = exports.getUserGroups = exports.createGroup = void 0;
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const createGroup = (req, res) => { };
+exports.createGroup = createGroup;
+const getUserGroups = (req, res) => { };
+exports.getUserGroups = getUserGroups;
+const getGroupById = (req, res) => { };
+exports.getGroupById = getGroupById;
+const getGroupEvent = (req, res) => { };
+exports.getGroupEvent = getGroupEvent;
+//# sourceMappingURL=group.controller.js.map
