3309e7544349edf4d119f0d17a2c16f608bc72b3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-11 11:37:41 +0100

    Added the generateToken test

--- tests/unit/generateToken.test.ts
+++ tests/unit/generateToken.test.ts
@@ -1,11 +1,12 @@
 import { generateToken } from "./../../src/utils/";
-
+import { describe, expect, test } from "@jest/globals";
 
 describe("Generate Token", () => {
-    it("Should generate a token", () => {
-        const id = "5c8e1b9f-c7f1-4578-bd6e-923832bdb903";
-        const token = generateToken(id);
-        expect(token).toBeDefined();
-        expect(token).toHaveLength(64);
-    })
-})
+  it("Should generate a token", () => {
+    const id = "5c8e1b9f-c7f1-4578-bd6e-923832bdb903";
+    const token = generateToken(id);
+    expect(token).toBeDefined();
+    expect(typeof token).toBe("string");
+    expect(token.length).toBeGreaterThan(100); 
+  });
+});
