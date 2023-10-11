17655518021b317ec813349155d8eb55d5344424
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-11 16:22:03 +0100

    Feat: Added swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import express, { Express } from 'express'
 import cors from 'cors'
 import { config } from 'dotenv'
+import swaggerUi from "swagger-ui-express"
 config()
 import index from './Routes/index'
 const PORT: number = Number(process.env.PORT) || 8080;
-//import connectDB from './config/connect.db'
+import * as YAML from "yaml";
+import swagger from '../swagger'
+
+// Parse the API documentation file.
+const swaggerDocument = YAML.parse(swagger);
+
+import connectDB from './config/connect.db'
 
 const app: Express = express()
 app.use(cors({ origin: '*' }))
 app.use(express.json())
 
-//connectDB()
+connectDB()
 
-app.use('/api', index)
+app.use('/api', index);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 
 const server = app.listen(PORT, () => {
