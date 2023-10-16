60656d22ef9e499788debfd990aa3d5cfac6b747
dyCodes <yusufdaudu51@gmail.com>
2023-10-16 03:34:33 +0100

    feat: update Google.auth redirectUri

--- src/config/google.config.ts
+++ src/config/google.config.ts
@@ -4,9 +4,8 @@ config();
 
 const clientId = process.env['GOOGLE_CLIENT_ID'];
 const clientSecret = process.env['GOOGLE_CLIENT_SECRET'];
-// const redirectUri = 'https://wetindeysup-api.onrender.com/api/auth/callback';
-// const redirectUri = 'http://localhost:8080/api/auth/callback';
-const redirectUri = 'http://localhost:3000';
+// const redirectUri = 'http://localhost:3000';
+const redirectUri = 'https://zuri-event-webapp2.vercel.app';
 
 const oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 
