384e08d703ba6744c2f6b53ee40fa09c12c3f8d1
dyCodes <yusufdaudu51@gmail.com>
2023-10-16 03:50:07 +0100

    feat: add UserPreference endpoints

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import auth from './auth.routes';
+import user from './user.routes';
 import event from './event.routes';
 import group from './group.routes';
 import comment from './comment.routes';
@@ -9,6 +10,7 @@ import comment from './comment.routes';
  * Main routes
  */
 router.use('/auth', auth);
+router.use('/user', user);
 router.use('/events', event);
 router.use('/groups', group);
 router.use('/comments', comment);
