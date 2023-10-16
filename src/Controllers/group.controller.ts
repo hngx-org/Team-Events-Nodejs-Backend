3e8eff568f512929942ff011392a64e33609b9f2
dyCodes <yusufdaudu51@gmail.com>
2023-10-16 03:29:04 +0100

    feat: add image upload to createGroup

--- src/Controllers/group.controller.ts
+++ src/Controllers/group.controller.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, User } from '@prisma/client';
+import cloudinary from '../config/cloudinaryConfig';
 import { Request, Response } from 'express';
 import Joi from 'joi';
 const prisma = new PrismaClient();
@@ -9,25 +10,31 @@ const createGroup = async (req: Request, res: Response) => {
 
 		const requestSchema = Joi.object({
 			group_name: Joi.string().required(),
-			emails: Joi.array(),
+			emails: Joi.any(),
 		});
 
 		const { error } = requestSchema.validate(req.body);
 		if (error) return res.status(400).json({ error: error.details[0].message });
 
+		let uploadedImage = '';
+		if (req.file) {
+			// File upload (cloudinary)
+			const { secure_url } = await cloudinary.uploader.upload(req.file.path);
+			uploadedImage = secure_url;
+		}
+
 		const { group_name, emails } = req.body;
-
-		//const { secure_url } = await await cloudinary.uploader.upload(req.file.path);
-
 		const newGroup = await prisma.group.create({
 			data: {
 				group_name,
 				created_by: userId,
+				image: uploadedImage,
 			},
 		});
 
-		if (emails && emails.length > 0) {
-			const userGroupCreatePromises = emails.map(async (email: string) => {
+		const emailArray = Array.isArray(emails) ? emails : JSON.parse(emails);
+		if (emailArray && emailArray.length > 0) {
+			const userGroupCreatePromises = emailArray.map(async (email: string) => {
 				await prisma.userGroup.create({
 					data: {
 						user: { connect: { email } },
