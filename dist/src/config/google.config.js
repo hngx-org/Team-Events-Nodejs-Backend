90353d23edca1c8ad11461ba300406357d1a463a
dyCodes <yusufdaudu51@gmail.com>
2023-10-18 02:57:07 +0100

    chore: build

--- dist/src/config/google.config.js
+++ dist/src/config/google.config.js
@@ -7,8 +7,8 @@ const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const clientId = process.env['GOOGLE_CLIENT_ID'];
 const clientSecret = process.env['GOOGLE_CLIENT_SECRET'];
-// const redirectUri = 'http://localhost:3000';
-const redirectUri = 'https://zuri-event-webapp2.vercel.app';
+// const redirectUri = 'https://zuri-event-webapp2.vercel.app';
+const redirectUri = 'https://event-tan-iota.vercel.app';
 const oauth2Client = new googleapis_1.google.auth.OAuth2(clientId, clientSecret, redirectUri);
 exports.oauth2Client = oauth2Client;
 const scopes = [
