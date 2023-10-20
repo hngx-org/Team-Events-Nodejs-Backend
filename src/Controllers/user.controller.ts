6e2a5c0d433df907eb7d2ce164f9bb85d7e260fc
Decagon <decagon@APPLEs-MacBook-Air.local>
2023-10-20 15:52:56 +0100

    new feature and bug fix

--- src/Controllers/user.controller.ts
+++ src/Controllers/user.controller.ts
@@ -35,6 +35,9 @@ const updateUserPreference = async (req: Request, res: Response) => {
 			networking_opportunities: Joi.boolean().required(),
 			email_notifications: Joi.boolean().required(),
 			push_notifications: Joi.boolean().required(),
+			allow_others_see_profile: Joi.boolean().required(),
+			event_details: Joi.boolean().required(),
+			anyone_can_add_to_group: Joi.boolean().required(),
 		});
 
 		const { error, value } = requestSchema.validate(req.body);
@@ -42,16 +45,45 @@ const updateUserPreference = async (req: Request, res: Response) => {
 
 		const userId = (req.user as User).id;
 
-		const userPreference = await prisma.userPreference.create({
-			data: {
+		const isUserPreferenceExist = await prisma.userPreference.findFirst({
+			where: {
 				user_id: userId,
-				event_update: value.event_updates,
-				Reminders: value.reminders,
-				networking_opportunities: value.networking_opportunities,
-				email_notifications: value.email_notifications,
-				push_notifications: value.push_notifications,
 			},
 		});
+
+		let userPreference;
+
+		if (!isUserPreferenceExist) {
+			userPreference = await prisma.userPreference.create({
+				data: {
+					user_id: userId,
+					event_update: value.event_updates,
+					Reminders: value.reminders,
+					networking_opportunities: value.networking_opportunities,
+					email_notifications: value.email_notifications,
+					push_notifications: value.push_notifications,
+					allow_others_see_profile: value.allow_others_see_profile,
+					event_details: value.event_details,
+					anyone_can_add_to_group: value.anyone_can_add_to_group,
+				},
+			});
+		} else {
+			userPreference = await prisma.userPreference.update({
+				where: {
+					id: isUserPreferenceExist.id,
+				},
+				data: {
+					event_update: value.event_updates,
+					Reminders: value.reminders,
+					networking_opportunities: value.networking_opportunities,
+					email_notifications: value.email_notifications,
+					push_notifications: value.push_notifications,
+					allow_others_see_profile: value.allow_others_see_profile,
+					event_details: value.event_details,
+					anyone_can_add_to_group: value.anyone_can_add_to_group,
+				},
+			});
+		}
 
 		return res.status(200).json({ message: 'User preference saved successfully', data: userPreference });
 	} catch (error) {
@@ -198,4 +230,51 @@ const updateUserProfile = async (req: Request, res: Response) => {
 	}
 };
 
-export { getUserPreference, updateUserPreference, changePassword, updateUserProfile };
+const getUserRegisteredEvents = async (req: Request, res: Response) => {
+	try {
+		const userId = (req.user as User).id;
+		const user = await prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+		});
+
+		if (!user) return res.status(400).json({ error: `User not found` });
+
+		const userEvents = await prisma.user.findUnique({
+			where: {
+				id: userId,
+			},
+			include: {
+				event: true,
+			},
+		});
+
+		if (!userEvents) {
+			return res.status(404).json({ error: 'User or Event not found' });
+		}
+
+		const userRegisteredEvents = userEvents.event;
+
+		// Respond with the user's created events
+		res.status(200).json({
+			statusCode: 200,
+			message: 'User events retrieved successfully',
+			data: userRegisteredEvents,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			message: `internet error`,
+			success: false,
+		});
+	}
+};
+
+export {
+	getUserPreference,
+	updateUserPreference,
+	changePassword,
+	updateUserProfile,
+	getUserRegisteredEvents,
+};
